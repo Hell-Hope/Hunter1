c6070284c5d3e17c6183a2b07fcc43453a71ce11
hellinnocence <hell.innocence@gmail.com>
2018-01-26 08:38:59 +0800

    manage user

--- Hunter.Models/Form/Edit.cs
+++ Hunter.Models/Form/Edit.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Hunter.Models.Form
@@ -7,12 +9,20 @@ namespace Hunter.Models.Form
     public class Edit
     {
 
+        [DisplayName("主键")]
+        [DataType(DataType.Text)]
         public string ID { get; set; }
 
+        [DisplayName("表单名称")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
+        [DisplayName("Html")]
+        [DataType(DataType.Html)]
         public string Html { get; set; }
 
+        [DisplayName("备注")]
+        [DataType(DataType.MultilineText)]
         public string Remark { get; set; }
 
     }
--- Hunter.Models/User/Edit.cs
+++ Hunter.Models/User/Edit.cs
@@ -12,17 +12,21 @@ namespace Hunter.Models.User
         public string ID { get; set; }
 
         [DisplayName("帐号")]
+        [DataType(DataType.Text)]
         [Required]
         public string Account { get; set; }
 
         [DisplayName("密码")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DisplayName("用户名")]
+        [DataType(DataType.Text)]
         [Required]
         public string Name { get; set; }
 
         [DisplayName("备注")]
+        [DataType(DataType.MultilineText)]
         public string Remark { get; set; }
 
     }
