2d253731d99c9c4e154740cbd0145da1597e7e42
hellinnocence <hell.innocence@gmail.com>
2018-02-02 10:01:11 +0800

    organization

---
+++ Hunter.Entities/Organization.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hunter.Entities
+{
+    /// <summary> 组织
+    /// </summary>
+    [BsonIgnoreExtraElements]
+    public class Organization : Entity
+    {
+        
+        public string Name { get; set; }
+
+        public string ParentID { get; set; }
+
+
+    }
+}
